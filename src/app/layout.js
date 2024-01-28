9c7ba9db8646b28d620f72d6f0a893f3031f3150
LoLoSenPai <loic.dlugosz@gmail.com>
2024-01-28 17:10:07 +0100

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body className={inter.className}>
+        <Theme>
+          <Navbar />
+          {children}
+        </Theme>
+      </body>
+    </html >
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed left-0 right-0 flex items-center justify-between w-full p-4 max-w-[1200px] mx-auto">
+      <div className="logo">
+        <Image src="/logo-kitto.png" alt="Kitto" width={120} height={(120 * 290) / 395} />
+      </div>
+      <div>
+        <Image src="/images/top-bar.png" alt="Top Bar" width={200} height={80} />
+      </div>
+      <div className="flex items-center space-x-4">
+        <img src="/images/discord-icon.png" alt="Discord" width={40} height={40} />
+        <img src="/images/x-icon.png" alt="Twitter" width={40} height={40} />
+        <img src="/images/atlas-icon.png" alt="Atlas3" width={40} height={40} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
