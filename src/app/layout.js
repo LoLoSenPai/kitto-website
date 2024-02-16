36a00c9ce6121b0f8e90f0038f10f6251d00a476
LoLoSenPai <loic.dlugosz@gmail.com>
2024-02-16 21:09:02 +0100

    Add metadata and head tags to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Head from 'next/head';
 import { Inter } from "next/font/google";
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
@@ -11,11 +12,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Kitto",
   description: "Looking for a cute cat pfp",
+  image: "https://pbs.twimg.com/profile_images/1755725702005927937/ohQkTn50_400x400.jpg",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Kitto</title>
+        <meta name="description" content="Looking for a cute cat pfp" />
+        <meta property="og:title" content="Kitto" />
+        <meta property="og:description" content="Looking for a cute cat pfp" />
+        <meta property="og:image" content="https://pbs.twimg.com/profile_images/1755725702005927937/ohQkTn50_400x400.jpg" />
+      </Head>
       <body className={inter.className}>
         <Theme>
           <Navbar />
