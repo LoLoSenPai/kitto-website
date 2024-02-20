ccb1eec027c2d6e98f4d36e1fb1ba1ad0944e506
LoLoSenPai <loic.dlugosz@gmail.com>
2024-02-20 17:01:01 +0100

    Refactor date parsing in TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,36 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { parseISO, formatDistanceToNow } from 'date-fns';
+import { parse, formatDistanceToNow } from 'date-fns';
 
 function isValidDate(dateString) {
     const regEx = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
     return dateString.match(regEx) != null;
 }
 
-function convertToISO(dateStr) {
-    try {
-        console.log('Received date string:', dateStr);
-        // Split the date string into parts
-        const [date, time] = dateStr.split(' ');
-        const [day, month, year] = date.split('/');
-        const [hours, minutes, seconds] = time.split(':');
-
-        const isoString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
-        return isoString;
-    } catch (error) {
-        console.error('Error converting to ISO:', error);
-        return null;
-    }
-}
-
 function parseDate(dateStr) {
-    const isoDateStr = convertToISO(dateStr);
-    console.log('Converted to ISO:', isoDateStr);
-    if (!isoDateStr || !isValidDate(isoDateStr)) {
-        console.error('Invalid date string, unable to convert to ISO or invalid ISO format:', dateStr);
+    try {
+        const [datePart, timePart] = dateStr.split(' ');
+        const [day, month, year] = datePart.split('/');
+        const [hours, minutes, seconds] = timePart.split(':');
+        const formattedDateStr = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}T${hours}:${minutes}:${seconds}`;
+        return parse(formattedDateStr, "yyyy-MM-dd'T'HH:mm:ss", new Date());
+    } catch (error) {
+        console.error('Error parsing date:', error);
         return null;
     }
-    return parseISO(isoDateStr);
 }
 
 const TransactionList = ({ transactions }) => {
@@ -47,6 +34,7 @@ const TransactionList = ({ transactions }) => {
                 <div className='w-full'>
                     {transactionListData.map((transaction, index) => {
                         const date = parseDate(transaction.timestamp);
+                        const distanceToNow = date ? formatDistanceToNow(date, { addSuffix: true }) : 'Unknown date';
                         const borderColor = index % 2 === 0 ? 'border-amber-900' : 'border-amber-700';
                         if (!date) {
                             // Handle the error, skip rendering this transaction, or render with a fallback date
@@ -59,7 +47,7 @@ const TransactionList = ({ transactions }) => {
                                 <Link href={`https://solscan.io/tx/${transaction.signature}`} target='_blank' rel='noopener noreferrer'>
                                     <div className='flex items-center justify-between py-4 mx-8 transition-transform duration-200 ease-in-out cursor-pointer hover:scale-105'>
                                         <span className=''>{transaction.description}</span>
-                                        <span className=''>{formatDistanceToNow(date, { addSuffix: true })}</span>
+                                        <span className=''>{distanceToNow}</span>
                                     </div>
                                 </Link>
                                 {index < transactionListData.length - 1 && (
