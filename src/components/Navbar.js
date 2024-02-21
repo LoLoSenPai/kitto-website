7b39e543b071936dd0f4922c0bbe7b12493a80ca
LoLoSenPai <loic.dlugosz@gmail.com>
2024-02-21 22:42:51 +0100

    Refactor Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav className={`transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'} fixed top-0 left-0 right-0 z-50 flex items-center justify-between w-full p-4 max-w-[1400px] mx-auto`}>
       <Link href="/">
-        <div className="relative inline-block w-32 h-20">
+        <div className="inline-flex items-center justify-center w-16 h-16 md:h-20 md:w-32">
           <img
             src="/logo-kitto.png"
             alt="Kitto"
@@ -34,16 +34,12 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div className="hidden sm:block w-[200px] h-auto">
-        <img src="/images/top-bar.png" alt="Top Bar" width={200} height={80} />
-      </div>
-      {/* <Link href="/wallet-checker">
-        <div className="relative hidden sm:block prev-button w-[200px] h-auto">
-          <Image src="/images/top-bar.png" alt="Top Bar" width={200} height={80} />
-          <span className="absolute text-lg top-1 left-10">Wallet Checker</span>
+      <Link href="/wallet-checker">
+        <div className="w-21 md:w-[180px] text-center bg-white/50 rounded-xl p-2">
+          <span className="text-lg font-bold">Airdrop Checker</span>
         </div>
-      </Link> */}
-      <div className="flex items-center space-x-4">
+      </Link>
+      <div className="items-center md:flex md:space-x-4 md:flex-row">
         <div className="icon-container">
           <a href="https://discord.gg/XtVTM3nNRa" target="_blank" rel="noopener noreferrer">
             <img src="/images/discord-icon.png" alt="Discord" width={40} height={40} />
@@ -56,12 +52,12 @@ const Navbar = () => {
             <img src="/images/x-icon-hover.png" alt="Twitter Hover" className="icon-hover" />
           </a>
         </div>
-        <div className="icon-container">
+        {/* <div className="icon-container">
           <a href="https://atlas3.io/project/kitto-or-free-mint" target="_blank" rel="noopener noreferrer">
             <img src="/images/atlas-icon.png" alt="Atlas3" width={40} height={40} />
             <img src="/images/atlas-icon-hover.png" alt="Atlas3 Hover" className="icon-hover" />
           </a>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
